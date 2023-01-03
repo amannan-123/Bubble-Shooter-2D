2f1f534063bc031866157706d8da7f2b3e7ddc0c
amannan-123 <tanveer6110@gmail.com>
2023-01-04 00:23:59 +0500

    Converted Swap to UI button.

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -14,17 +14,21 @@ public class Shooter : MonoBehaviour
 	private float lookAngle;
 	private GameObject line;
 	private GameObject limit;
+	private LineRenderer lineRenderer;
+	private Vector2 gizmosPoint;
 
 	public void Awake()
 	{
 		line = GameObject.FindGameObjectWithTag("Line");
 		limit = GameObject.FindGameObjectWithTag("Limit");
+		//lineRenderer = line.GetComponent<LineRenderer>();
 	}
 
 	public void Update()
 	{
 		if (GameManager.instance.gameState == "play")
 		{
+			gizmosPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			lookDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
 			lookAngle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
 
@@ -39,6 +43,9 @@ public class Shooter : MonoBehaviour
 				&& LevelManager.instance.GetBubbleAreaChildCount() > 0)
 				{
 					line.SetActive(true);
+
+					//cast a ray between transform position and mouse position
+					//CastRay(transform.position, gizmosPoint);
 				}
 			}
 			else
@@ -57,6 +64,37 @@ public class Shooter : MonoBehaviour
 		}
 	}
 
+	// private void CastRay(Vector2 pos, Vector2 dir)
+	// {
+	// 	int RayCount = 2;
+	// 	lineRenderer.positionCount = RayCount;
+
+	// 	lineRenderer.SetPosition(0, pos);
+
+	// 	for (int i = 1; i < RayCount; i++)
+	// 	{
+	// 		RaycastHit2D hit = Physics2D.Raycast(pos, dir - (Vector2)transform.position, 300);
+
+	// 		if (hit.collider != null && hit.transform.tag.Equals("Wall"))
+	// 		{
+	// 			lineRenderer.SetPosition(i, hit.point);
+
+	// 			pos = hit.point - dir * 0.01f;
+	// 			dir = Vector3.Reflect(dir, hit.normal);
+
+	// 			if (RayCount < 5)
+	// 			{
+	// 				RayCount += 1;
+	// 				lineRenderer.positionCount = RayCount;}
+	// 		}
+	// 		else if (hit.collider != null && hit.transform.tag.Equals("Bubble"))
+	// 		{
+	// 			lineRenderer.SetPosition(i, hit.point);
+	// 			break;
+	// 		}
+	// 	}
+	// }
+
 	public void Shoot()
 	{
 		if (currentBubble == null) CreateNextBubble();
@@ -65,8 +103,10 @@ public class Shooter : MonoBehaviour
 		transform.rotation = Quaternion.Euler(0f, 0f, lookAngle - 90f);
 		currentBubble.transform.rotation = transform.rotation;
 		currentBubble.GetComponent<CircleCollider2D>().enabled = true;
-		currentBubble.GetComponent<Rigidbody2D>().AddForce(currentBubble.transform.up * speed, ForceMode2D.Impulse);
-		currentBubble.GetComponent<Rigidbody2D>().collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+		Rigidbody2D rb = currentBubble.GetComponent<Rigidbody2D>();
+		rb.AddForce(currentBubble.transform.up * speed, ForceMode2D.Impulse);
+		rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+		rb.gravityScale = 0;
 		currentBubble = null;
 	}
 
